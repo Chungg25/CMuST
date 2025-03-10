3177ccf4d841610af418375a8fc39e6be6d47b28
kiet <chungkiet31@gmail.com>
2025-03-10 13:53:51 +0700

    a

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
         ffn_dim=args.ffn_dim,
         n_heads=args.n_heads,
         dropout=args.dropout
-    )
+    ).to(device)
     
     # load pre-trained model weights
     # model.load_state_dict(torch.load('2024-04-18-10-18-28.pt'))
@@ -95,128 +95,139 @@ if __name__ == "__main__":
     total_params = sum(param.nelement() for param in model.parameters())
     logger.info(f'The number of parameters: {total_params}')
     
-    # Rolling Adaption
-    threshold = args.threshold
+    # # Rolling Adaption
+    # threshold = args.threshold
     
-    # train task 1
-    logger.info('Train for task 1')
-    save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[0],now)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    # load prompt
-    load_prompt_weights(model, os.path.join(task_dirs[0], f"prompt.pth"))
-    # start train
-    model, _ = train(model, device, train_loaders[0], val_loaders[0], scalers[0], 
-                    optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
-    # test model
-    test(model, device, test_loaders[0], scalers[0], logger=logger)
-    # save prompt and model
-    save_prompt_weights(model, os.path.join(save_dir, f"start_prompt.pth"))
-    torch.save(model.state_dict(), os.path.join(save_dir, f"start.pt"))
-    # update weight list
-    weight_histories = {name: [] for name, param in model.named_parameters()}
-    for name, param in model.named_parameters():
-        weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
+    # # train task 1
+    # logger.info('Train for task 1')
+    # save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[0],now)
+    # if not os.path.exists(save_dir):
+    #     os.makedirs(save_dir)
+    # # load prompt
+    # load_prompt_weights(model, os.path.join(task_dirs[0], f"prompt.pth"))
+    # # start train
+    # model, _ = train(model, device, train_loaders[0], val_loaders[0], scalers[0], 
+    #                 optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
+    # # test model
+    # test(model, device, test_loaders[0], scalers[0], logger=logger)
+    # # save prompt and model
+    # save_prompt_weights(model, os.path.join(save_dir, f"start_prompt.pth"))
+    # torch.save(model.state_dict(), os.path.join(save_dir, f"start.pt"))
+    # # update weight list
+    # weight_histories = {name: [] for name, param in model.named_parameters()}
+    # for name, param in model.named_parameters():
+    #     weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
     
-    # train to task k
-    for i, task in enumerate(tasks):
-        if i == 0:
-            continue
-        # reset train
-        optimizer = Adam(filter(lambda p : p.requires_grad, model.parameters()), 
-                         lr=args.lr*0.01, weight_decay=args.weight_decay, eps=1e-8)
-        scheduler = MultiStepLR(optimizer, milestones=[500], gamma=args.gamma)
-        # train task 2 to k
-        logger.info(f'Train for task {i+1}')
-        save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[i],now)
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        # load prompt
-        load_prompt_weights(model, os.path.join(task_dirs[i], f"prompt.pth"))
-        # start train
-        model, w_list = train(model, device, train_loaders[i], val_loaders[i], scalers[i], 
-                        optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
-        # append weight
-        for w in w_list:
-            for name, param in w.items():
-                weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
-        # cal var & frozen
-        for name, param in model.named_parameters():
-            if param.requires_grad == True:
-                variances = calculate_variance(weight_histories[name])
-                if 'prompt' not in name and np.all(variances < threshold):
-                    param.requires_grad = False
-        # print frozen params
-        trainable_params = sum(param.nelement() for param in filter(lambda p: p.requires_grad, model.parameters()))
-        frozen_params = total_params - trainable_params
-        logger.info('Total/Frozen Parameters: {}/{}'.format(total_params, frozen_params))
-        # test model
-        test(model, device, test_loaders[i], scalers[i], logger=logger)
-        # save prompt and model
-        save_prompt_weights(model, os.path.join(save_dir, f"first_round_prompt.pth"))
-        torch.save(model.state_dict(), os.path.join(save_dir, f"first_round.pt"))
-        # update weight list
-        weight_histories = {name: [] for name, param in model.named_parameters()}
-        for name, param in model.named_parameters():
-            weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
+    # # train to task k
+    # for i, task in enumerate(tasks):
+    #     if i == 0:
+    #         continue
+    #     # reset train
+    #     optimizer = Adam(filter(lambda p : p.requires_grad, model.parameters()), 
+    #                      lr=args.lr*0.01, weight_decay=args.weight_decay, eps=1e-8)
+    #     scheduler = MultiStepLR(optimizer, milestones=[500], gamma=args.gamma)
+    #     # train task 2 to k
+    #     logger.info(f'Train for task {i+1}')
+    #     save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[i],now)
+    #     if not os.path.exists(save_dir):
+    #         os.makedirs(save_dir)
+    #     # load prompt
+    #     load_prompt_weights(model, os.path.join(task_dirs[i], f"prompt.pth"))
+    #     # start train
+    #     model, w_list = train(model, device, train_loaders[i], val_loaders[i], scalers[i], 
+    #                     optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
+    #     # append weight
+    #     for w in w_list:
+    #         for name, param in w.items():
+    #             weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
+    #     # cal var & frozen
+    #     for name, param in model.named_parameters():
+    #         if param.requires_grad == True:
+    #             variances = calculate_variance(weight_histories[name])
+    #             if 'prompt' not in name and np.all(variances < threshold):
+    #                 param.requires_grad = False
+    #     # print frozen params
+    #     trainable_params = sum(param.nelement() for param in filter(lambda p: p.requires_grad, model.parameters()))
+    #     frozen_params = total_params - trainable_params
+    #     logger.info('Total/Frozen Parameters: {}/{}'.format(total_params, frozen_params))
+    #     # test model
+    #     test(model, device, test_loaders[i], scalers[i], logger=logger)
+    #     # save prompt and model
+    #     save_prompt_weights(model, os.path.join(save_dir, f"first_round_prompt.pth"))
+    #     torch.save(model.state_dict(), os.path.join(save_dir, f"first_round.pt"))
+    #     # update weight list
+    #     weight_histories = {name: [] for name, param in model.named_parameters()}
+    #     for name, param in model.named_parameters():
+    #         weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
 
-    # train task 1
-    # reset train
-    optimizer = Adam(filter(lambda p : p.requires_grad, model.parameters()), 
-                        lr=args.lr*0.01, weight_decay=args.weight_decay, eps=1e-8)
-    scheduler = MultiStepLR(optimizer, milestones=[500], gamma=args.gamma)
-    logger.info(f'Train for task 1')
-    save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[0],now)
-    # load prompt
-    load_prompt_weights(model, os.path.join(save_dir, f"start_prompt.pth"))
-    # start train
-    model, w_list = train(model, device, train_loaders[0], val_loaders[0], scalers[0], 
-                    optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
-    # append weight
-    for w in w_list:
-        for name, param in w.items():
-            weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
-    # cal var & frozen
-    for name, param in model.named_parameters():
-        if param.requires_grad == True:
-            variances = calculate_variance(weight_histories[name])
-            if 'prompt' not in name and np.all(variances < threshold):
-                param.requires_grad = False
-    # print frozen params
-    trainable_params = sum(param.nelement() for param in filter(lambda p: p.requires_grad, model.parameters()))
-    frozen_params = total_params - trainable_params
-    logger.info('Total/Frozen Parameters: {}/{}'.format(total_params, frozen_params))
-    # test model
-    test(model, device, test_loaders[0], scalers[0], logger=logger)
-    # save prompt and model
-    save_prompt_weights(model, os.path.join(save_dir, f"first_round_prompt.pth"))
-    torch.save(model.state_dict(), os.path.join(save_dir, f"first_round.pt"))
+    # # train task 1
+    # # reset train
+    # optimizer = Adam(filter(lambda p : p.requires_grad, model.parameters()), 
+    #                     lr=args.lr*0.01, weight_decay=args.weight_decay, eps=1e-8)
+    # scheduler = MultiStepLR(optimizer, milestones=[500], gamma=args.gamma)
+    # logger.info(f'Train for task 1')
+    # save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[0],now)
+    # # load prompt
+    # load_prompt_weights(model, os.path.join(save_dir, f"start_prompt.pth"))
+    # # start train
+    # model, w_list = train(model, device, train_loaders[0], val_loaders[0], scalers[0], 
+    #                 optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
+    # # append weight
+    # for w in w_list:
+    #     for name, param in w.items():
+    #         weight_histories[name].append(copy.deepcopy(param.data).cpu().numpy())
+    # # cal var & frozen
+    # for name, param in model.named_parameters():
+    #     if param.requires_grad == True:
+    #         variances = calculate_variance(weight_histories[name])
+    #         if 'prompt' not in name and np.all(variances < threshold):
+    #             param.requires_grad = False
+    # # print frozen params
+    # trainable_params = sum(param.nelement() for param in filter(lambda p: p.requires_grad, model.parameters()))
+    # frozen_params = total_params - trainable_params
+    # logger.info('Total/Frozen Parameters: {}/{}'.format(total_params, frozen_params))
+    # # test model
+    # test(model, device, test_loaders[0], scalers[0], logger=logger)
+    # # save prompt and model
+    # save_prompt_weights(model, os.path.join(save_dir, f"first_round_prompt.pth"))
+    # torch.save(model.state_dict(), os.path.join(save_dir, f"first_round.pt"))
     
-    # fine-tuning
-    logger.info(f'Fine Tuning')
-    weights_star_dict = copy.deepcopy(model.state_dict())
+    # # fine-tuning
+    # logger.info(f'Fine Tuning')
+    # weights_star_dict = copy.deepcopy(model.state_dict())
     
+    # for i, task in enumerate(tasks):
+    #     # load init weights
+    #     model.load_state_dict(weights_star_dict)
+    #     # reset train
+    #     optimizer = Adam(filter(lambda p : p.requires_grad, model.parameters()), 
+    #                      lr=args.lr, weight_decay=args.weight_decay, eps=1e-8)
+    #     scheduler = MultiStepLR(optimizer, milestones=args.steps, gamma=args.gamma)
+    #     logger.info(f'Train for task {i+1}')
+    #     save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[i],now)
+    #     if not os.path.exists(save_dir):
+    #         os.makedirs(save_dir)
+    #     # load prompt
+    #     load_prompt_weights(model, os.path.join(save_dir, f"first_round_prompt.pth"))
+    #     # start train
+    #     model, _ = train(model, device, train_loaders[i], val_loaders[i], scalers[i], 
+    #                     optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
+    #     # test model
+    #     test(model, device, test_loaders[i], scalers[i], logger=logger)
+    #     # save prompt and model
+    #     save_prompt_weights(model, os.path.join(save_dir, f"fine_tuning_prompt.pth"))
+    #     torch.save(model.state_dict(), os.path.join(save_dir, f"fine_tuning.pt"))
+        
+    # # test model
+    # # test(model, device, test_loader, scaler, logger=logger)
+
     for i, task in enumerate(tasks):
-        # load init weights
-        model.load_state_dict(weights_star_dict)
-        # reset train
-        optimizer = Adam(filter(lambda p : p.requires_grad, model.parameters()), 
-                         lr=args.lr, weight_decay=args.weight_decay, eps=1e-8)
-        scheduler = MultiStepLR(optimizer, milestones=args.steps, gamma=args.gamma)
-        logger.info(f'Train for task {i+1}')
-        save_dir = 'checkpoints/{}/{}/{}/'.format(args.dataset,tasks[i],now)
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        # load prompt
-        load_prompt_weights(model, os.path.join(save_dir, f"first_round_prompt.pth"))
-        # start train
+        logger.info(f'Training task {i+1}: {task}')
+        save_dir = f'checkpoints/{args.dataset}/{task}/{now}/'
+        os.makedirs(save_dir, exist_ok=True)
         model, _ = train(model, device, train_loaders[i], val_loaders[i], scalers[i], 
                         optimizer, scheduler, criterion, args.max_epochs, args.patience, logger=logger)
-        # test model
-        test(model, device, test_loaders[i], scalers[i], logger=logger)
-        # save prompt and model
-        save_prompt_weights(model, os.path.join(save_dir, f"fine_tuning_prompt.pth"))
-        torch.save(model.state_dict(), os.path.join(save_dir, f"fine_tuning.pt"))
-        
-    # test model
-    # test(model, device, test_loader, scaler, logger=logger)
+
+        torch.save(model.state_dict(), os.path.join(save_dir, "model.pt"))
+    
+    logger.info("Training complete!")
